599ecd6699922ebd04914d0168f951ea49ac0467
Enrique Núñez <nenrique0112@gmail.com>
2023-11-05 23:40:15 -0400

    Added new endpoint and pipes

--- CountryApp/src/app/countries/pages/country-page/country-page.component.ts
+++ CountryApp/src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { CountriesService } from "../../services/countries.service";
+import { switchMap } from "rxjs";
+import { Country } from "../../interfaces/country";
 
 @Component({
   selector: 'app-country-page',
@@ -6,6 +10,32 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private service: CountriesService,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.service.searchCountryByAlphaCode(id)),
+      )
+      .subscribe( (country: Country | null)=> {
+        if (!country) {
+          return this.router.navigateByUrl('')
+        }
+        console.log('Tenemos un pais');
+        return;
+      });
+  }
+
+  private searchCountry(code: string) {
+    this.service.searchCountryByAlphaCode(code).subscribe(
+      countries => {
+        console.log({ countries });
+      }
+    );
+  }
 }
--- CountryApp/src/app/countries/services/countries.service.ts
+++ CountryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import {catchError, Observable, of} from "rxjs";
+import { catchError, map, Observable, of } from "rxjs";
 import { Country } from "../interfaces/country";
 
 @Injectable({
@@ -10,12 +10,22 @@ export class CountriesService {
   private baseUrl: string = 'https://restcountries.com/v3.1';
   constructor(private httpClient: HttpClient) { }
 
+  searchCountryByAlphaCode( alphaCode: string ): Observable<Country | null> {
+    const alphaUrl: string = `${this.baseUrl}/alpha/${alphaCode}`;
+    return this.httpClient
+      .get<Country[]>(alphaUrl)
+      .pipe(
+        map(countries => countries.length > 0 ? countries[0] : null),
+        catchError(error => of(null)) // Catch the error and return a new empty observable
+      );
+  }
+
   searchCapital( capital: string ): Observable<Country[]> {
     const capitalUrl: string = `${this.baseUrl}/capital/${capital}`;
     return this.httpClient
       .get<Country[]>(capitalUrl)
       .pipe(
-        catchError( error => of([])) // Catch the error and return a new empty observable
+        catchError(error => of([])) // Catch the error and return a new empty observable
       );
   }
 
